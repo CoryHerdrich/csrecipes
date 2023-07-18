fc1e503c073088635abce38d6cf2e7e7884e78e3
Cory Herdrich <128417591+CoryHerdrich@users.noreply.github.com>
2023-07-18 18:34:22 -0400

    initial push

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node server.js
+/node_modules
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>C's Recipes</h1>
+        <h2>All your Recipes in one Spot</h2>
+      </div>
+    );
+  }
+}
